9fad7a7093ce1552dbccf55dded3abf958fba937
Maksim Frankov <2m.franko@gmail.com>
2022-06-03 19:36:58 +0300

    я списал =(

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,16 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector('.open-menu'),
-    closeMenuBtn: document.querySelector('.menu-closed'),
+    openMenuBtn: document.querySelector('.menu-open-btn'),
+    closeMenuBtn: document.querySelector('.menu-close-btn'),
     menu: document.querySelector('.mob-menu'),
-    // body: document.querySelector('.body'),
+    body: document.querySelector('.body'),
   };
+
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
-    // refs.body.classList.toggle('no-scroll');
+    refs.body.classList.toggle('no-scroll');
   }
 })();
